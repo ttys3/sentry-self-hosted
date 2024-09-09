ca3196b7cc3e328160d2075eb6ac1520e6f9e679
deng <rust@maildog.top>
2024-09-09 13:57:25 +0000

    fix: fix compatibility with podman-compose
    Refs:

--- install/dc-detect-version.sh
+++ install/dc-detect-version.sh
@@ -11,9 +11,9 @@ echo "${_group}Initializing Docker Compose ..."
 # To support users that are symlinking to docker-compose
 dc_base="$(docker compose version &>/dev/null && echo 'docker compose' || echo 'docker-compose')"
 if [[ "$(basename $0)" = "install.sh" ]]; then
-  dc="$dc_base --ansi never --env-file ${_ENV}"
+  dc="$dc_base --no-ansi --env-file ${_ENV}"
 else
-  dc="$dc_base --ansi never"
+  dc="$dc_base --no-ansi"
 fi
 proxy_args="--build-arg http_proxy=${http_proxy:-} --build-arg https_proxy=${https_proxy:-} --build-arg no_proxy=${no_proxy:-}"
 dcr="$dc run --rm"
